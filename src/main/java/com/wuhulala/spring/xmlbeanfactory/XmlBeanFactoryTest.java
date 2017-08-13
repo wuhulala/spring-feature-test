4184f09f7dfd97092a80f59127aa8a8f55b81ac5
coding <xueaohui_com@163.com>
2017-08-13 18:14:34 +0800

    Add : spring把bean发布到jmx通道中

--- src/main/java/com/wuhulala/spring/xmlbeanfactory/XmlBeanFactoryTest.java
+++ src/main/java/com/wuhulala/spring/xmlbeanfactory/XmlBeanFactoryTest.java
@@ -1,18 +1,24 @@
 package com.wuhulala.spring.xmlbeanfactory;
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.TimeUnit;
+
 /**
- * 功能说明: com.wuhulala.spring.xmlbeanfactory<br>
+ * 功能说明: 测试xml环境配置加载beanFactory<br>
  * 注意事项: <br>
  * 系统版本: v1.0<br>
  * 开发人员: xueah20964<br>
  * 开发时间: 2017/8/12<br>
  */
 public class XmlBeanFactoryTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:xml-bean-factory.xml");
+        //context.addApplicationListener();
         context.start();
+        while(true){
+            TimeUnit.SECONDS.sleep(5);
+            System.out.println("i am still alive.....");
+        }
     }
 }
