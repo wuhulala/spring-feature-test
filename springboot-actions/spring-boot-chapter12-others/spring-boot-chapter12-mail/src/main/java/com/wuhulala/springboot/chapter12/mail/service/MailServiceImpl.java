a9135a385b7f9e4b223efd0258177948a88b2fe0
wuhulala <xueaohui_com@163.com>
2019-04-13 18:08:58 +0800

    添加 邮件发送示例

---
+++ springboot-actions/spring-boot-chapter12-others/spring-boot-chapter12-mail/src/main/java/com/wuhulala/springboot/chapter12/mail/service/MailServiceImpl.java
@@ -0,0 +1,32 @@
+package com.wuhulala.springboot.chapter12.mail.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailServiceImpl implements MailService {
+
+    @Autowired
+    private MailSender mailSender;
+
+    @Override
+    public void send(String address, String content) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom("xxxx@163.com");
+        msg.setSubject("hello");
+        msg.setTo(address);
+        msg.setText(content);
+        try{
+            this.mailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+
+
+}
